ee742d4b4a3f6fb1b59e2989321591f0304bf0e2
Juan Carlos Torres del Castillo <jctorresdelcastillo@gmail.com>
2026-02-13 15:27:54 -0500

    cambios

--- resources/views/detalle-producto.blade.php
+++ resources/views/detalle-producto.blade.php
@@ -4,7 +4,7 @@
 @include('partials.menu')
 
 @php
-    $imgs = $item->images ? json_decode($item->images, true) : [];
+    $imgs = $product->images ? json_decode($product->images, true) : []
 @endphp
 
 <div class="container py-5">
